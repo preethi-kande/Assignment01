6622282efa6e289c87ca613def0ee1d488e0b09d
Preethi Kande <kpa.236@gmail.com>
2026-06-16 15:55:49 -0700

    Added some code for exercises

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//STEP 1 - Declaring variables with camelCase naming convention
+let someMonth
+function theMonth() {}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+//STEP 2 - Examples of literal expressions
+let numericLiteral = 66;
+let stringLiteral = "JS, Assignment 1";
+let booleanLiteral = true;
+let nullLiteral = null;
+
+//STEP 3 - Examples of variable or complex expressions
+let num = 23;
+let variableExpression = ((num + 6) * 5);
+console.log(variableExpression);
+let a = 3, b = 5;
+let complexExpression = Math.pow(a + b,2);
+console.log(complexExpression);
+
+//STEP 4 - Declare variables with camelCase and Hungarian naming conventions
+// camelCase naming convention
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+// Hungarian naming convention
+let strFirstName;
+let strLastName;
+let strAddress;
+let strCity;
+let strState;
+let strZipCode;
+let intYourAge;
+let strReferralSource;
+let boolMayWeContactYou;
+
+//STEP 5 - Declare and Assign values to the 3 variables in 3 different ways
+// Way 1: Declare and assign in separate statements
